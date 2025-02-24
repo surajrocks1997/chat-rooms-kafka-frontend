0c1f22d9fc8a7a5a3fce2409b97d1f439c8cf262
Suraj Gupta <surajrocks1997@gmail.com>
2025-02-24 14:17:05 +0530

    updated the way refreshtoken endpoint was being called. websocket provider updated to retry attempt every 3 second indefinitely

--- src/config/WebSocketProvider.jsx
+++ src/config/WebSocketProvider.jsx
@@ -16,9 +16,12 @@ export const useWebSocket = () => useContext(WebSocketContext);
 export const WebSocketProvider = ({ children }) => {
     const [stompClient, setStompClient] = useState(null);
     const reconnectAttemptRef = useRef(0);
-    const maxReconnectAttemps = 5;
+    // const maxReconnectAttemps = 5;
+    const isConnectingRef = useRef(false);
 
     const connect = useCallback(() => {
+        if (isConnectingRef.current) return;
+        isConnectingRef.current = true;
         const token = localStorage.getItem("token");
 
         const socket = new SockJS("http://localhost:8082/chat-rooms");
@@ -34,21 +37,30 @@ export const WebSocketProvider = ({ children }) => {
             () => {
                 console.log("Connected to WebSocket Server");
                 reconnectAttemptRef.current = 0;
+                isConnectingRef.current = false;
                 setStompClient(client);
             },
             (error) => {
                 console.error("Websocket Connection Error: ", error);
-                if (reconnectAttemptRef.current < maxReconnectAttemps) {
-                    const timeout =
-                        Math.pow(2, reconnectAttemptRef.current) * 1000;
-                    setTimeout(() => {
-                        reconnectAttemptRef.current += 1;
-                        connect();
-                    }, timeout);
-                } else
-                    console.error(
-                        "Max Reconnection Attempts Reached. Please Try Refreshing the Page"
-                    );
+                console.log(new Date());
+                isConnectingRef.current = false;
+
+                setTimeout(() => {
+                    reconnectAttemptRef.current += 1;
+                    connect();
+                }, 3 * 1000);
+
+                // if (reconnectAttemptRef.current < maxReconnectAttemps) {
+                //     const timeout = Math.pow(2, reconnectAttemptRef.current) * 1000;
+                //     setTimeout(() => {
+                //         reconnectAttemptRef.current += 1;
+                //         connect();
+                //     }, timeout);
+                // } else {
+                //     console.error(
+                //         "Max Reconnection Attempts Reached. Please Try Refreshing the Page"
+                //     );
+                // }
             }
         );
     }, []);
